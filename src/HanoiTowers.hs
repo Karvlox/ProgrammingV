5ac5fe689a19af01a37e15409513351524bab5df
Carlos Antonio Ballester Panigua <carlos.ballester.10387210@usalesiana.edu.bo>
2024-02-19 23:32:39 -0400

    Homework Hanoi Towers

---
+++ src/HanoiTowers.hs
@@ -0,0 +1,66 @@
+module HanoiTowers where
+
+-- Función para resolver el problema de las Torres de Hanoi
+-- Toma como entrada el número de discos y los nombres de los palos de origen, auxiliar y destino
+hanoi :: Int -> String -> String -> String -> [(String, String)]
+hanoi 0 _ _ _ = []  -- Si no hay discos, no hay movimientos
+hanoi n source aux dest = -- Mover n discos desde el palo de origen a la pila de destino, utilizando la pila auxiliar
+    hanoi (n-1) source dest aux ++   -- Mover n-1 discos de la pila de origen a la pila auxiliar, utilizando la pila de destino como auxiliar
+    [(source, dest)] ++  -- Mover el disco restante de la pila de origen a la pila de destino
+    hanoi (n-1) aux source dest  -- Mover los n-1 discos desde la pila auxiliar a la pila de destino, utilizando la pila de origen como auxiliar
+
+-- Función para imprimir los movimientos realizados
+-- Definición de la función printMoves que toma una lista de tuplas de String y realiza IO ()
+printMoves :: [(String, String)] -> IO ()
+printMoves [] = return () -- Si la lista de movimientos está vacía, no hay nada que imprimir, así que devolvemos la acción IO ()
+printMoves ((source, dest):moves) -- Desestructuramos la lista de movimientos para obtener el primer movimiento y el resto de la lista
+    | not (null source) && not (null dest) = do -- Verificamos si tanto el origen como el destino no son cadenas vacías
+        putStrLn $ "Mover disco desde " ++ source ++ " hacia " ++ dest -- Si se cumple la condición, imprimimos el movimiento actual
+        printMoves moves -- Llamamos recursivamente a printMoves con el resto de los movimientos
+    -- Si no se cumple la condición anterior, es decir, si al menos uno de los extremos es una cadena vacía
+    | otherwise = printMoves moves -- Llamamos recursivamente a printMoves con el resto de los movimientos
+
+
+-- Función para resolver las Torres de Hanoi e imprimir los movimientos
+solveHanoi :: Int -> String -> String -> String -> IO ()
+solveHanoi n source aux dest = do -- Dada la cantidad de discos y los nombres de los palos, resolver las Torres de Hanoi y luego imprimir los movimientos
+    let moves = hanoi n source aux dest  -- Calcular los movimientos necesarios para resolver las Torres de Hanoi
+    printMoves moves  -- Imprimir los movimientos
+
+
+
+
+
+
+
+main :: IO ()
+main = do
+    putStrLn "Caso de prueba 1:"
+    solveHanoi 3 "A" "B" "C"
+
+    putStrLn "\nCaso de prueba 2:"
+    solveHanoi 1 "A" "B" "C"
+
+    putStrLn "\nCaso de prueba 3:"
+    solveHanoi 5 "A" "B" "C"
+
+    putStrLn "\nCaso de prueba 4:"
+    solveHanoi 0 "A" "B" "C"
+
+    putStrLn "\nCaso de prueba 5:"
+    solveHanoi 4 "A" "B" "C"
+
+    putStrLn "\nCaso de prueba 6:"
+    solveHanoi 2 "A" "B" "C"
+
+    putStrLn "\nCaso de prueba 7:"
+    solveHanoi 6 "A" "B" "C"
+
+    putStrLn "\nCaso de prueba 8:"
+    solveHanoi 7 "A" "B" "C"
+
+    putStrLn "\nCaso de prueba 9:"
+    solveHanoi 2 "A" "C" "B"
+
+    putStrLn "\nCaso de prueba 10:"
+    solveHanoi 5 "A" "C" "B"
