a6d4a5114269b2039e426c98ca152ef262b90e2a
Carlos Antonio Ballester Panigua <carlos.ballester.10387210@usalesiana.edu.bo>
2024-02-21 23:38:14 -0400

    Add test

--- test/TestHanoiTowers.hs
+++ test/TestHanoiTowers.hs
@@ -1,22 +1,22 @@
 import Test.QuickCheck
-import Hanoi
+import HanoiTowers
 
 -- | Verifica que el número de movimientos es correcto para n discos.
 prop_hanoiMovesLength :: Int -> Property
 prop_hanoiMovesLength n =
-  n >= 0 ==> length (hanoiMoves n "A" "C" "B") == 2^n - 1
+  n >= 0 ==> length (hanoi n "A" "B" "C") == 2^n - 1
 
 -- | Verifica que los movimientos son válidos para n discos.
 prop_hanoiMovesValid :: Int -> Property
 prop_hanoiMovesValid n =
-  n >= 0 ==> all isValidMove (hanoiMoves n "A" "C" "B")
+  n >= 0 ==> all isValidMove (hanoi n "A" "B" "C")
   where
     isValidMove (from, to) = from `elem` ["A", "B", "C"] && to `elem` ["A", "B", "C"] && from /= to
 
 -- | Verifica que los movimientos generados resuelven correctamente el problema para n discos.
 prop_hanoiSolution :: Int -> Property
 prop_hanoiSolution n =
-  n >= 0 ==> and $ map isSolutionMove (hanoiMoves n "A" "C" "B")
+  n >= 0 ==> and $ map isSolutionMove (hanoi n "A" "B" "C")
   where
     isSolutionMove ("A", "C") = True
     isSolutionMove ("C", "A") = True
@@ -29,9 +29,9 @@ prop_hanoiSolution n =
 -- | Ejecutar todas las pruebas
 main :: IO ()
 main = do
-  putStrLn "Probando la longitud de hanoiMoves:"
+  putStrLn "Probando la longitud de hanoi:"
   quickCheck prop_hanoiMovesLength
-  putStrLn "Probando la validez de los movimientos en hanoiMoves:"
+  putStrLn "Probando la validez de los movimientos en hanoi:"
   quickCheck prop_hanoiMovesValid
   putStrLn "Probando si los movimientos resuelven el problema:"
   quickCheck prop_hanoiSolution
