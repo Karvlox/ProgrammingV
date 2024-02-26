9c5e3f3b53c85cd899150ef5b0232cc308a8f9e8
Carlos Antonio Ballester Panigua <carlos.ballester.10387210@usalesiana.edu.bo>
2024-02-26 10:38:40 -0400

    Tarea de 26 de febrero

---
+++ src/26Feb/Homewrok.hs
@@ -0,0 +1,11 @@
+module PatternMatching where
+
+    parethesizedFuntion :: String -> [String]
+    parethesizedFuntion "" = []
+    
+    
+    split :: String -> Char -> [String]
+    split [] _ = []
+    split (x:xs) c
+        | x == c = split xs c
+        | otherwise = (x : takeWhile (/= c) xs) : split (dropWhile (/= c) xs) c
